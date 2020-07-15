cd78786b254735fa574361dee8c13b3c61f3df31
vedatbilaloglu <vedatbilaloglu@hotmail.com>
2020-07-15 13:17:07 +0300

    character location sınıfı eklendi ve karaktere harita üzerinde hareket imkanı verildi engel kısmı çözüldü

---
+++ src/main/java/com/mycompany/starwars/CharacterLocation.java
@@ -0,0 +1,31 @@
+
+package com.mycompany.starwars;
+
+
+public class CharacterLocation {
+    
+    private int characterX=6;
+    private int characterY=5;
+    
+    public CharacterLocation(){
+        
+    }
+
+    public int getCharacterX() {
+        return characterX;
+    }
+
+    public void setCharacterX(int characterX) {
+        this.characterX = characterX;
+    }
+
+    public int getCharacterY() {
+        return characterY;
+    }
+
+    public void setCharacterY(int characterY) {
+        this.characterY = characterY;
+    }
+    
+    
+}
--- src/main/java/com/mycompany/starwars/PlayGround.java
+++ src/main/java/com/mycompany/starwars/PlayGround.java
@@ -18,7 +18,33 @@ import javax.swing.JPanel;
 
 public class PlayGround extends JPanel implements KeyListener, ActionListener{
     
-    private BufferedImage image, imageB, imageC, imageD, imageE;
+    
+    int[][] array = {       {0,0,0,0,0,1,0,0,0,0,0},            
+                            {0,1,1,1,1,1,1,1,1,1,0},
+                            {0,1,0,0,0,0,0,0,0,1,0},
+                            {0,1,1,1,1,1,0,1,1,1,0},
+                            {1,1,1,1,0,1,1,1,0,1,1},
+                            {0,0,1,0,0,1,0,1,0,1,0},
+                            {0,1,1,1,1,1,1,1,0,1,0},
+                            {0,1,0,0,0,0,0,1,0,1,0},
+                            {0,1,1,1,1,1,1,1,1,1,0},
+                            {0,1,0,1,1,0,0,0,0,1,0},
+                            {0,1,0,0,0,0,1,0,0,1,0},
+                            {0,1,0,1,1,0,1,0,1,1,0},
+                            {1,1,1,1,1,1,1,1,1,1,0},
+                            {0,0,0,0,0,1,0,0,0,1,0},                           
+                    };
+    
+    private BufferedImage image, imageB, imageC, imageD, imageE, imageKupa, imageFire, imagePath;
+    private BufferedImage imageAsagiOk, imageYukarıOk, imageSagOk, imageSolOk, imageAsagiOkTwo;
+    private BufferedImage imageMasterYoda;
+    
+    private int masterYodaX=364;
+    private int masterYodaY=295;
+    
+    CharacterLocation characterLocation = new CharacterLocation();
+    
+    
     
     public PlayGround(){
         
@@ -28,6 +54,19 @@ public class PlayGround extends JPanel implements KeyListener, ActionListener{
             imageC = ImageIO.read(new FileImageInputStream(new File("charfi.jpg")));
             imageD = ImageIO.read(new FileImageInputStream(new File("dharfi.png")));
             imageE = ImageIO.read(new FileImageInputStream(new File("eharfi.jpg")));
+            
+            imageAsagiOk = ImageIO.read(new FileImageInputStream(new File("asagiok.jpg")));
+            imageAsagiOkTwo = ImageIO.read(new FileImageInputStream(new File("asagiok2.jpg")));
+            imageSagOk = ImageIO.read(new FileImageInputStream(new File("sagok.jpg")));
+            imageSolOk = ImageIO.read(new FileImageInputStream(new File("solok.jpg")));
+            imageYukarıOk = ImageIO.read(new FileImageInputStream(new File("yukarıok.jpg")));
+            
+            imageKupa = ImageIO.read(new FileImageInputStream(new File("kupa.jpeg")));
+            imageFire = ImageIO.read(new FileImageInputStream(new File("fire.jpg")));
+            imagePath = ImageIO.read(new FileImageInputStream(new File("path.jpg")));
+            
+            imageMasterYoda = ImageIO.read(new FileImageInputStream(new File("masteryoda.png")));
+            
         } catch (IOException ex) {
             Logger.getLogger(PlayGround.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -49,37 +88,27 @@ public class PlayGround extends JPanel implements KeyListener, ActionListener{
         super.paint(g); //To change body of generated methods, choose Tools | Templates.
         g.setColor(Color.red);
         
-        int[][] array = {   {0,0,0,0,0,1,0,0,0,0,0},            
-                            {0,1,1,1,1,1,1,1,1,1,0},
-                            {0,1,0,0,0,0,0,0,0,1,0},
-                            {0,1,1,1,1,1,0,1,1,1,0},
-                            {1,1,1,1,0,1,1,1,0,1,1},
-                            {0,0,1,0,0,1,0,1,0,1,0},
-                            {0,1,1,1,1,1,1,1,0,1,0},
-                            {0,1,0,0,0,0,0,1,0,1,0},
-                            {0,1,1,1,1,1,1,1,0,1,0},
-                            {0,1,0,1,1,0,0,0,0,1,0},
-                            {0,1,0,0,0,0,1,0,0,1,0},
-                            {0,1,0,1,1,0,1,0,1,1,0},
-                            {1,1,1,1,1,1,1,1,1,1,0},
-                            {0,0,0,0,0,1,0,0,0,1,0},                           
-                         };
         
-        for (int i = 0; i < 14; i++) {
-            for (int j = 0; j < 11; j++) {
-                if(array[i][j] == 0){
-                    g.fillRect(120+(i*41),90+(j*41), 41, 41);
-                }             
-            }
-        }
+        
         for (int i = 0; i < 14; i++) {
             for (int j = 0; j < 11; j++) {
                     g.setColor(Color.black);
                     g.drawRect(120+(i*41),90+(j*41), 41, 41);
-                
-                
+                              
             }
         }
+        
+        for (int i = 0; i < 14; i++) {
+            for (int j = 0; j < 11; j++) {
+                if(array[i][j] == 0){
+                    //g.fillRect(120+(i*41),90+(j*41), 41, 41);
+                    g.drawImage(imageFire, 120+(i*41), 90+(j*41), imageFire.getWidth(), imageFire.getHeight(),this);
+                }else{
+                    g.drawImage(imagePath, 120+(i*41), 90+(j*41), imagePath.getWidth(), imagePath.getHeight(),this);
+                }          
+            }
+        }
+        
         
         g.drawImage(image, 120, 295, image.getWidth(), image.getHeight(),this);
         g.drawImage(imageB, 284, 91, imageB.getWidth(), imageB.getHeight(),this);
@@ -87,6 +116,15 @@ public class PlayGround extends JPanel implements KeyListener, ActionListener{
         g.drawImage(imageD, 653, 295, imageD.getWidth(), imageD.getHeight(),this);
         g.drawImage(imageE, 284, 500, imageE.getWidth(), imageE.getHeight(),this);
         
+        g.drawImage(imageSagOk, 80, 295, imageSagOk.getWidth(), imageSagOk.getHeight(),this);
+        g.drawImage(imageAsagiOk, 284, 50, imageAsagiOk.getWidth(), imageAsagiOk.getHeight(),this);
+        g.drawImage(imageAsagiOkTwo, 612, 50, imageAsagiOkTwo.getWidth(), imageAsagiOkTwo.getHeight(),this);
+        g.drawImage(imageSolOk, 693, 295, imageSolOk.getWidth(), imageSolOk.getHeight(),this);
+        g.drawImage(imageYukarıOk, 284, 541, imageYukarıOk.getWidth(), imageYukarıOk.getHeight(),this);
+        
+        g.drawImage(imageKupa, 695, 459, imageKupa.getWidth(), imageKupa.getHeight(),this);
+        
+        g.drawImage(imageMasterYoda, masterYodaX, masterYodaY, imageMasterYoda.getWidth(), imageMasterYoda.getHeight(),this);
         
     }
 
@@ -96,6 +134,69 @@ public class PlayGround extends JPanel implements KeyListener, ActionListener{
 
     @Override
     public void keyPressed(KeyEvent e) {
+        
+        int value = e.getKeyCode();
+        
+        if(value == KeyEvent.VK_LEFT){
+            System.out.println(characterLocation.getCharacterX());
+            System.out.println(characterLocation.getCharacterY());
+            if(array[characterLocation.getCharacterX()-1][characterLocation.getCharacterY()] == 0){
+                
+            }else{
+                masterYodaX-=41;
+                characterLocation.setCharacterX(characterLocation.getCharacterX()-1);
+                System.out.println("X Kordinati: "+characterLocation.getCharacterX());
+                System.out.println(masterYodaX);
+            }
+            
+        }
+        else if(value == KeyEvent.VK_RIGHT){
+            System.out.println(characterLocation.getCharacterX());
+            System.out.println(characterLocation.getCharacterY());
+            if(array[characterLocation.getCharacterX()+1][characterLocation.getCharacterY()] == 0){
+                
+            }else{
+                masterYodaX+=41;
+                characterLocation.setCharacterX(characterLocation.getCharacterX()+1);
+                System.out.println("X Kordinati: "+characterLocation.getCharacterX());
+                System.out.println(masterYodaX);
+            }
+        }
+        else if(value == KeyEvent.VK_UP){
+            /*
+            if(masterYodaY < 173){
+                masterYodaY=131;
+            }else{
+                masterYodaY-=41;
+                System.out.println(masterYodaY);
+            }
+*/
+            System.out.println(characterLocation.getCharacterX());
+            System.out.println(characterLocation.getCharacterY());
+            if(array[characterLocation.getCharacterX()][characterLocation.getCharacterY()-1] == 0){
+                
+            }else{
+                masterYodaY-=41;
+                characterLocation.setCharacterY(characterLocation.getCharacterY()-1);
+                System.out.println("Y Kordinati: "+characterLocation.getCharacterY());
+                System.out.println(masterYodaY);
+            }
+            
+        }
+        else if(value == KeyEvent.VK_DOWN){
+            
+            System.out.println(characterLocation.getCharacterX());
+            System.out.println(characterLocation.getCharacterY());
+            if(array[characterLocation.getCharacterX()][characterLocation.getCharacterY()+1] == 0){
+                
+            }else{
+                masterYodaY+=41;
+                characterLocation.setCharacterY(characterLocation.getCharacterY()+1);
+                System.out.println("Y Kordinati: "+characterLocation.getCharacterY());
+                System.out.println(masterYodaY);
+            }
+        }
+        repaint();
     }
 
     @Override
@@ -104,6 +205,7 @@ public class PlayGround extends JPanel implements KeyListener, ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
+       
     }
     
     
